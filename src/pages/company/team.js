c8950b03c1cbfdfddd981c6508b8887d18edc6ea
Eric Smith <ericsmith.100@gmail.com>
2019-05-30 21:39:03 -0400

    Add new static pages about and team

---
+++ src/pages/company/team.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import Layout from '../../components/layout'
+
+const Team = () => (
+  <Layout>
+    <div>
+      <h1>About The Team</h1>
+      <p>
+        sriracha brunch gluten-free banh mi keffiyeh tofu stumptown DIY quinoa
+        Pitchfork small batch vegan tote bag fixie Blue Bottle master cleanse
+        hashtag Tonx leggings retro biodiesel drinking vinegar distillery Carles
+        Intelligentsia Pinterest Echo Park semiotics iPhone selvage Cosby
+        sweater Shoreditch chambray keytar shabby chic blog McSweeney's bespoke
+        Vice 90's freegan artisan beard Helvetica American Apparel normcore
+        disrupt Marfa Brooklyn Kickstarter Tumblr
+      </p>
+    </div>
+  </Layout>
+)
+
+export default Team
